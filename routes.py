73ae8f143473715f0df3251c3864e325565b51ce
AndUm <edg-andres@hotmail.com>
2022-04-13 13:22:17 -0500

    db with sqlalchemy

--- routes.py
+++ routes.py
@@ -1,18 +1,24 @@
-from crypt import methods
-from app import app
-from flask import render_template
+from app import app, db
+from flask import render_template, redirect, url_for
+from models import Task
 
 import forms
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html')
+    tasks = Task.query.all()
+    return render_template('index.html', tasks=tasks)
 
 @app.route('/about', methods=['GET', 'POST'])
 def about():
     form = forms.AddTaskForm()
     if form.validate_on_submit():
-        print('Submitted title', form.title.data)
-        return render_template('about.html', form=form, title=form.title.data)
-    return render_template('about.html', form=form)
+        t = Task(title=form.title.data, date=datetime.utcnow())
+        db.session.add(t) # save to database
+        db.session.commit() # This is the line that actually adds the task to the database
+    print('Submitted title', form.title.data)
+    return redirect(url_for('index'))
+    #return render_template('about.html', form=form, title=form.title.data)
+    
+    #return render_template('about.html', form=form)
