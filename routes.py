8cac8a87ca3b6d9fbd8e687c1d13f84176662204
AndUm <edg-andres@hotmail.com>
2022-04-13 15:28:02 -0500

    UI enhanced

--- routes.py
+++ routes.py
@@ -1,24 +1,61 @@
 from app import app, db
-from flask import render_template, redirect, url_for
+from flask import render_template, redirect, url_for, flash, get_flashed_messages, request
 from models import Task
+from datetime import datetime
 
+import models
 import forms
 
 @app.route('/')
 @app.route('/index')
 def index():
-    tasks = Task.query.all()
+    tasks = models.Task.query.all()
     return render_template('index.html', tasks=tasks)
 
-@app.route('/about', methods=['GET', 'POST'])
-def about():
+@app.route('/add', methods=['GET', 'POST'])
+def add():
     form = forms.AddTaskForm()
     if form.validate_on_submit():
-        t = Task(title=form.title.data, date=datetime.utcnow())
-        db.session.add(t) # save to database
+        task = models.Task(title=form.title.data, date=datetime.utcnow())
+        db.session.add(task) # save to database
         db.session.commit() # This is the line that actually adds the task to the database
-    print('Submitted title', form.title.data)
+        
+        flash('Task added!')
+        #print('Submitted title', form.title.data)
+        #print('Submitted title', form.title.data)
+        #return render_template('about.html', form=form, title=form.title.data)
+        return redirect(url_for('index'))
+    return render_template('add.html', form=form)
+
+@app.route('/edit/<int:task_id>', methods=['GET', 'POST'])
+def edit(task_id):
+    form = forms.AddTaskForm()
+    task = models.Task.query.get(task_id)
+    print(task)
+    if task:
+        if form.validate_on_submit():
+            task.title = form.title.data
+            task.date = datetime.utcnow()
+            db.session.commit()
+            flash('Task updated')
+            return redirect(url_for('index'))
+        form.title.data = task.title
+        return render_template('edit.html', form=form, task_id=task_id)
+    flash(f'Task with id {task_id} does not exit')
     return redirect(url_for('index'))
-    #return render_template('about.html', form=form, title=form.title.data)
-    
-    #return render_template('about.html', form=form)
+
+
+@app.route('/delete/<int:task_id>', methods=['GET', 'POST'])
+def delete(task_id):
+    form = forms.DeleteTaskForm()
+    task = models.Task.query.get(task_id)
+    if task:
+        if form.validate_on_submit():
+            if form.submit.data:
+                db.session.delete(task)
+                db.session.commit()
+                flash('Task deleted')
+            return redirect(url_for('index'))
+        return render_template('delete.html', form=form, task_id=task_id, title=task.title)
+    flash(f'Task with id {task_id} does not exit')
+    return redirect(url_for('index'))
